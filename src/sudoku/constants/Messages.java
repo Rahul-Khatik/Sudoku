58008cd64a06a967f2848aa3d73bfa19a4f392c6
Rahul-Khatik <rahulkhatik9574@gmail.com>
2024-11-04 15:03:12 +0530

    Setup Constants

---
+++ src/sudoku/constants/Messages.java
@@ -0,0 +1,7 @@
+package sudoku.constants;
+
+public class Messages {
+    public  static final String GAME_COMPLETED = "Congratulations, you have won the Game! New Game?";
+    public  static final String ERROR = "An error has occured.";
+
+}
